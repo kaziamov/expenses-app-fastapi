bef3170cb6801bec9b9386f948c1b564642dab67
Ilia Kaziamov <kaziamov@outlook.com>
2023-11-08 20:57:44 +0300

    Update create db connect logic

--- expenses_app/db/connect.py
+++ expenses_app/db/connect.py
@@ -1,6 +1,20 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
-from ..settings import ASYNC_DATABASE_URL
+from .. import settings
 
-async_engine = create_async_engine(ASYNC_DATABASE_URL)
-AsyncSession = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
+def get_engine():
+    if settings.FLAG__NOT_PRODUCTION:
+        return create_async_engine(settings.ASYNC_TEST_DATABASE_URL)
+    return create_async_engine(settings.ASYNC_DATABASE_URL)
+
+
+def get_sessionmaker():
+    if settings.FLAG__NOT_PRODUCTION:
+        return async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+    return async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
+
+async_engine = get_engine()
+Session = get_sessionmaker()
