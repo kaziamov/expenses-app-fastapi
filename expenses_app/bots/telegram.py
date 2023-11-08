72969d5c9667b010bdf846ad0bb62c153f514a09
Ilia Kaziamov <kaziamov@outlook.com>
2023-11-08 20:57:29 +0300

    Update create bot logic

--- expenses_app/bots/telegram.py
+++ expenses_app/bots/telegram.py
@@ -2,8 +2,15 @@ import logging
 from aiogram import Bot, Dispatcher
 
 from .. import settings
+from ..utils.patching import enable_on_production_only
 
 logger = logging.getLogger(__name__)
 
-telegram_bot = Bot(token=settings.BOT_TOKEN, parse_mode="HTML")
+
+@enable_on_production_only
+def get_tgbot():
+    return Bot(token=settings.BOT_TOKEN, parse_mode="HTML")
+
+
+telegram_bot = get_tgbot()
 telegram_dispather = Dispatcher()
