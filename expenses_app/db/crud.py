5149ff04f6b8d413d1a4e03524e3a5fb7766f122
Ilia Kaziamov <kaziamov@outlook.com>
2023-11-08 20:58:03 +0300

    Rename variable

--- expenses_app/db/crud.py
+++ expenses_app/db/crud.py
@@ -4,18 +4,19 @@ from typing import List, Dict
 from cachetools import LFUCache
 from sqlalchemy import select
 
-from ..db import AsyncSession
+from ..db import Session
 from ..models import BaseSQLModel
 from ..settings import CACHE_SIZE
 
 logger = logging.getLogger(__name__)
 cache = LFUCache(maxsize=CACHE_SIZE)
 
+
 async def create_obj(model: BaseSQLModel, **params) -> BaseSQLModel:
     """Create object in db"""
     logger.debug(f"{__name__}.create_obj: model = {model}, params = {params}")
     new_obj = model(**params)
-    async with AsyncSession() as conn:
+    async with Session() as conn:
         conn.add(new_obj)
         await conn.commit()
         await conn.refresh(new_obj)
@@ -27,17 +28,18 @@ async def get_obj(model: BaseSQLModel, **filters) -> BaseSQLModel:
     key = f"{model.__name__}{filters}"
     query = select(model).filter_by(**filters)
     logger.debug(f"{__name__}.get_obj: query = {query}")
-    async with AsyncSession() as conn:
+    async with Session() as conn:
         result = await conn.execute(query)
     logger.debug(f"{__name__}.get_obj: result = {result}")
     obj = result.scalars().first()
     logger.debug(f"{__name__}.get_obj: obj = {obj}")
     return obj
 
+
 async def get_all(model: BaseSQLModel) -> List[BaseSQLModel]:
     """Get objects from db"""
     query = select(model)
-    async with AsyncSession() as conn:
+    async with Session() as conn:
         result = await conn.execute(query)
     logger.debug(f"{__name__}.get_all: result = {result}")
     objects = result.scalars().all()
@@ -48,18 +50,19 @@ async def get_all(model: BaseSQLModel) -> List[BaseSQLModel]:
 async def get_all_with_filter(model: BaseSQLModel, filters: dict) -> List[BaseSQLModel]:
     """Get objects from db"""
     query = select(model).filter_by(**filters)
-    async with AsyncSession() as conn:
+    async with Session() as conn:
         result = await conn.execute(query)
     logger.debug(f"{__name__}.get_all: result = {result}")
     objects = result.scalars().all()
     logger.debug(f"{__name__}.get_all: obj = {objects}")
     return objects
 
+
 async def get_objects(model: BaseSQLModel, filters: Dict, limit=10, offset=10) -> List[BaseSQLModel]:
     """Get objects from db"""
     query = select(model).filter_by(**filters).limit(limit).offset(offset)
     logger.debug(f"{__name__}.get_objects: query = {query}")
-    async with AsyncSession() as conn:
+    async with Session() as conn:
         result = await conn.execute(query)
     logger.debug(f"{__name__}.get_objects: result = {result}")
     objects = result.scalars().all()
@@ -83,7 +86,7 @@ async def get_or_create(model: BaseSQLModel, **params):
 
 
 async def update_obj(model: BaseSQLModel, id: int, **params) -> BaseSQLModel:
-    async with AsyncSession() as conn:
+    async with Session() as conn:
         obj = await get_obj(model, id=id)
         for key, value in params.items():
             setattr(obj, key, value)
